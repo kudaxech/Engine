170027e0db9126937eb2cf8d8aad69b965f5b895
kudaxech <55214057+kudaxech@users.noreply.github.com>
2020-03-06 00:04:18 +0300

    Create Testing_of_first_code.cpp

---
+++ Testing_of_first_code.cpp
@@ -0,0 +1,20 @@
+#include "DataStorage.h"
+#include <iostream>
+
+
+int main(){
+
+    DataStorage bank;
+
+    bank.createObject("gun");
+    bank.createObject("11111");
+    bank.createObject("22222");
+
+    if(bank.deleteObject("gun")){
+
+        std::cout << "delete successful"<< std::endl;
+
+    }
+
+    return 0;
+}
