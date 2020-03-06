34898f474b9b94ddec786f32856dd35c6b122643
kudaxech <55214057+kudaxech@users.noreply.github.com>
2020-03-06 23:29:45 +0300

    Update Testing_of_first_code.cpp

--- Testing_of_first_code.cpp
+++ Testing_of_first_code.cpp
@@ -15,6 +15,13 @@ int main(){
         std::cout << "delete successful"<< std::endl;
 
     }else{std::cout << "error"<< std::endl;}
+    
+    
+    GameObject test_object;
+    test_object.addComponent<TestType>();
+
+    TestType* test_comp = test_object.getComponent<TestType>();
+    std::cout << test_comp->test_string << std::endl;
 
     return 0;
 }
