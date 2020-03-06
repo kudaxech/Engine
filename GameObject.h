b3bb4631c78d08d6f37987078ff2ed8d372e4a56
kudaxech <55214057+kudaxech@users.noreply.github.com>
2020-03-06 23:28:36 +0300

    Create GameObject.h

---
+++ GameObject.h
@@ -0,0 +1,32 @@
+#ifndef __GAME_OBJECT__
+#define __GAME_OBJECT__
+
+#include "Component.h"
+#include <string>
+#include <vector>
+
+class GameObject
+{
+public:
+    template <typename T>
+    void addComponent()
+    {
+        components.push_back(new T);
+    }
+
+    template <typename T>
+    T* getComponent()
+    {
+	std::string name = typeid(T).name();
+        for (Component* comp: components)
+        {
+            if (comp->name == name)
+                return static_cast<T*>(comp);
+        }
+        return nullptr;
+    }
+private:
+    std::vector<Component*> components;
+};
+
+#endif
